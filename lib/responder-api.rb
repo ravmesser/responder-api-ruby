e34f76be3ab2bdf06232365aa529debc88da1b80
itamardavidyan <itamardavidyan@gmail.com>
2018-11-11 15:59:19 +0200

    auth responder api

--- lib/responder-api.rb
+++ lib/responder-api.rb
@@ -1,5 +1,32 @@
+require 'rubygems'
+require 'oauth'
+require 'json'
+require 'optparse'
+require 'pp'
+
 class Responder
-    def self.hi
-      puts "Hello world!"
-    end
+  def initialize(ck, cs, uk, us)
+    consumer = OAuth::Consumer.new(ck,cs, :site => "http://api.responder.co.il")
+    @access_token = OAuth::AccessToken.new(consumer, uk, us)
   end
+
+  def get_lists
+    response = @access_token.request(:get, "/v1.0/lists")
+    rsp = JSON.parse(response.body)
+    return rsp
+  end
+
+  def create_list
+    
+  end
+
+  def edit_list(id)
+    
+  end
+
+  def delete_list(id)
+    
+  end
+
+
+end
